76ddc073f2dd6bd26435702ffe306a241aba582c
Standalone G.A.S <coder2k@mail.ru>
2020-12-23 14:30:07 +0300

    добавлены динамические категории на страница блога

--- wordpress/wp-content/themes/SportIsland/index.php
+++ wordpress/wp-content/themes/SportIsland/index.php
@@ -47,24 +47,20 @@
       <div class="wrapper">
         <h2 class="categories__h main-heading"> категории </h2>
         <ul class="categories-list">
-          <li class="category">
-            <a href="category.html" class="category__link">
-              <img src="img/blog__category_thmb1.jpg" alt="" class="category__thumb">
-              <span class="category__name">Груповые занятия</span>
-            </a>
-          </li>
-          <li class="category">
-            <a href="category.html" class="category__link">
-              <img src="img/blog__category_thmb2.jpg" alt="" class="category__thumb">
-              <span class="category__name">Кардио</span>
-            </a>
-          </li>
-          <li class="category">
-            <a href="category.html" class="category__link">
-              <img src="img/blog__category_thmb3.jpg" alt="" class="category__thumb">
-              <span class="category__name">Йога</span>
-            </a>
-          </li>
+
+          <?php
+          $categories = get_categories();
+          if (!empty($categories)):
+            foreach ($categories as $category): ?>
+
+              <li class="category">
+                <a href="<?=get_category_link($category->term_id)?>" class="category__link">
+                  <img src="<?= _si_assets_path("img/blog__category_thmb1.jpg")?>" alt="<?= $category->name ?>" class="category__thumb">
+                  <span class="category__name"><?= $category->name ?></span>
+                </a>
+              </li>
+
+            <?php endforeach; endif; ?>
         </ul>
       </div>
     </section>
